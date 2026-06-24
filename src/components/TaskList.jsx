1bbab00f38858132c9263fc2c4531c38ae758d7f
Gayathri-SR <gayathrisr855@gmail.com>
2026-06-24 21:51:05 +0530

    Adding empty state

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,6 +1,10 @@
 import TaskItem from "./TaskItem";
 
 function TaskList({tasks, toggleTask, deleteTask}) {
+    if (tasks.length === 0) {
+        return <p>No tasks yet.</p>;
+    }
+
     return(
         <div>
             {tasks.map(task => (
